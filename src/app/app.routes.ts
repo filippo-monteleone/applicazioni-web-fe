b39dda3394dc16053fe93633e0fca3a35a061c1c
Filippo Monteleone <136260618+filippo-monteleone@users.noreply.github.com>
2024-05-23 11:25:51 +0200

    added dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Admin dashboard',
+  },
+];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import { Location } from '@angular/common';
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', free: true },
+  { position: 2, name: 'Helium', free: true },
+  { position: 3, name: 'Lithium', free: true },
+  { position: 4, name: 'Beryllium', free: true },
+  { position: 5, name: 'Boron', free: false },
+  { position: 6, name: 'Carbon', free: true },
+  { position: 7, name: 'Nitrogen', free: true },
+  { position: 8, name: 'Oxygen', free: true },
+  { position: 9, name: 'Fluorine', free: true },
+  { position: 10, name: 'Neon', free: true },
+];
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  free: boolean;
+}
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    MatButtonToggleModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIcon,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  selectedVal: string | undefined;
+  price: number | undefined;
+  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'name'];
+  constructor(private _location: Location) {}
+
+  ngOnInit() {
+    this.selectedVal = 'carparks';
+    this.price = 5;
+  }
+  public onValChange(val: string) {
+    console.log(this.selectedVal);
+    this.selectedVal = val;
+  }
+
+  back() {
+    this._location.back();
+  }
+}
